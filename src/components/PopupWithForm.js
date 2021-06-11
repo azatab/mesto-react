7153b35aa62cfe3d85112615522abe03849e5878
Azat Abrarov <ufasat@yandex.ru>
2021-06-12 00:05:02 +0500

    hotfix: use internal validation

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 function PopupWithForm(props) {
   return (
-    <div className= {`${props.isOpen ? `popup popup-${props.name} popup_is-opened` : `popup popup-${props.name}`}`}>
-          <div className="popup__container">
-            <h2 className="popup__title">{props.title}</h2>
-            <form className="form" name={`${props.name}-form`} onSubmit = {props.onSubmit} noValidate>
-              <fieldset className="form__fieldset">
-                {props.children}
-                <button className="button form__save" type="submit" id="save" aria-label="сохранить">{props.buttonLabel}</button>
-              </fieldset>
-            </form>
-            <button className="button popup__close" type="button" id="popup-close" aria-label="закрыть" onClick={props.onClose}></button>
-          </div>
-        </div>
+    <div className= {`popup popup-${props.name} ${props.isOpen ? 'popup_is-opened' : ''}`}>
+      <div className="popup__container">
+        <h2 className="popup__title">{props.title}</h2>
+        <form className="form" name={`${props.name}-form`} onSubmit = {props.onSubmit} >
+          <fieldset className="form__fieldset">
+            {props.children}
+            <button className="button form__save" type="submit" aria-label="сохранить">{props.buttonLabel}</button>
+          </fieldset>
+        </form>
+        <button className="button popup__close" type="button" aria-label="закрыть" onClick={props.onClose}></button>
+      </div>
+    </div>
   )
 }
 
